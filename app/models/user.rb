e5bc07b0658ca7dc335b2466b766e5022ed2a68a
vkwong <vkwong@andrew.cmu.edu>
2015-03-30 14:47:43 -0700

    Added tests for user first name, last name, and profile correctness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ActiveRecord::Base
   # Setup accessible for protected attributes for your model 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+    uniqueness: true,
+    format: {
+      with: /a-zA-Z0-9_-/,
+      message: 'Must be formatted correctly.'
+    }
+
   has_many :statuses
 
   def full_name
