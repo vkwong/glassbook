93a19b921f42c3f20e61c1972f4e0a8eb3bfd216
vkwong <vkwong@andrew.cmu.edu>
2015-04-03 17:01:20 -0700

    Added custom routes for auth and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+  
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+    get 'edit', to: 'devise/registrations#edit', as: :edit
+  end
+
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
